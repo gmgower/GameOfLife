db9536b78f5b6a5313ec6576294be0fffe020f8a
Mada Gower <gmgower@gmail.com>
2020-06-25 22:59:45 -0500

    Render grid world.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,64 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 
-class App extends React.Component {
+// Size of board
+const totalWorldRows = 25;
+const totalWorldColumns = 25;
 
+// The function's parameter defaults to less than 40% chance of being alive
+const newWorldStatus = (cellStatus = () => Math.random() < 0.4) => {
+  const grid = [];
+  // The number of arrays within the main array will match the number of rows
+  for (let row = 0; row < totalWorldRows; row++) {
+    grid[row] = [];
+    // the number of values within each of these arrays will match the number of columns
+    for (let column = 0; column < totalWorldColumns; column++) {
+      // Each boolean value will represent that state of each cell: 'alive' or 'dead'
+      grid[row][column] = cellStatus();
+    }
+  }
+  return grid;
+};
+
+// Func that receives that state of the whole board status and toggle method
+const WorldGrid = ({ worldStatus, onToggleCellStatus }) => {
+  // Method that allows users to toggle the status of individual cells as props
+  const handleClick = (row, column) => onToggleCellStatus(row, column);
+
+  // Each cell is represented by a table's <td> tag
+  const tr = [];
+  for (let row = 0; row < totalWorldRows; row++) {
+    const td = [];
+    for (let column = 0; column < totalWorldColumns; column++) {
+      // Clicking on cell call method being called with the cell's row and column location as arguments
+      td.push(
+        <td
+          key={`${row}, ${column}`}
+          // className attr whose value is dependent on the boolean value of the corresponding board cell
+          className={worldStatus[row][column] ? 'alive' : 'dead'}
+          onClick={() => handleClick(row, column)}
+        />
+      );
+    }
+    tr.push(<tr key={row}>{td}</tr>);
+  }
+  return (
+    <table>
+      <tbody>{tr}</tbody>
+    </table>
+  );
+};
+
+class App extends React.Component {
+  state = {
+    // When game starts, the world's cells status will be return by the func that generates a new board status
+    worldStatus: newWorldStatus(),
+
+  };
 
   render = () => {
-    // Grid to display cell
+    const { worldStatus } = this.state;
+
     return (
       <div className='App'>
         <h1>Game of Life</h1>
@@ -19,24 +72,15 @@ class App extends React.Component {
         <div className='container'>
           <div>
             <div>
-              <button type='button'>
-                Start
-              </button>
-              <button type='button'>
-                Stop
-              </button>
-              <button>
-                Next
-              </button>
-              <button type='button'>
-                Clear
-              </button>
-              <button type='button'>
-                Reset
-              </button>
+              <button type='button'>Start</button>
+              <button type='button'>Stop</button>
+              <button>Next</button>
+              <button type='button'>Clear</button>
+              <button type='button'>Reset</button>
             </div>
             <div className='World'>
               <h3>Generation: 0</h3>
+              <WorldGrid  worldStatus={worldStatus}/>
             </div>
           </div>
         </div>
